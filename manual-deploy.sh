983dddfd8a96301ef010a8393d2ac6778cc74476
satyajit <satyajit.1418@gmail.com>
2026-08-26 19:36:10 +0530

    add manual deploy script

---
+++ manual-deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+NAMESPACE="astroops"
+
+kubectl get namespace "$NAMESPACE" >/dev/null 2>&1 || \
+kubectl create namespace "$NAMESPACE"
+
+echo "Installing service accounts"
+
+kubectl apply -f serviceaccounts.yaml -n "$NAMESPACE"
+
+echo "Service accounts installed."
+
+echo "Installing Helm chart for AstroOps in namespace $NAMESPACE"
+
+helm upgrade --install accounting ./accounting --namespace "$NAMESPACE"
+helm upgrade --install ad ./ad --namespace "$NAMESPACE"
+helm upgrade --install cart ./cart --namespace "$NAMESPACE"
+helm upgrade --install checkout ./checkout --namespace "$NAMESPACE"
+helm upgrade --install currency ./currency --namespace "$NAMESPACE"
+helm upgrade --install email ./email --namespace "$NAMESPACE"
+helm upgrade --install flagd ./flagd --namespace "$NAMESPACE"
+helm upgrade --install frauddetection ./frauddetection --namespace "$NAMESPACE"
+helm upgrade --install frontend ./frontend --namespace "$NAMESPACE"
+helm upgrade --install frontendproxy ./frontendproxy --namespace "$NAMESPACE"
+helm upgrade --install imageprovider ./imageprovider --namespace "$NAMESPACE"
+helm upgrade --install kafka ./kafka --namespace "$NAMESPACE"
+helm upgrade --install loadgenerator ./loadgenerator --namespace "$NAMESPACE"
+helm upgrade --install payment ./payment --namespace "$NAMESPACE"
+helm upgrade --install productcatalog ./productcatalog --namespace "$NAMESPACE"
+helm upgrade --install quote ./quote --namespace "$NAMESPACE"
+helm upgrade --install recommendation ./recommendation --namespace "$NAMESPACE"
+helm upgrade --install shipping ./shipping --namespace "$NAMESPACE"
+helm upgrade --install valkey ./valkey --namespace "$NAMESPACE"
+
+echo "Helm chart installation completed."
+
+helm list --namespace "$NAMESPACE"
+
+echo "Checking pods..."
+
+kubectl get pods -n "$NAMESPACE"
